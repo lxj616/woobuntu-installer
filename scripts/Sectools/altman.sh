f2d445073fd35d03ca66e634e7cab945be787201
lxj616 <502307678@qq.com>
2015-12-24 11:08:02 +0800

    moved altman to git , for later clone to local - lxj616

--- scripts/Sectools/altman.sh
+++ scripts/Sectools/altman.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 mkdir -p /opt/woobuntu
-mv data/altman /opt/woobuntu
+cd /opt/woobuntu
+git clone https://github.com/lxj616/altman-prebuilt-x86_64.git
+mv altman-prebuilt-x86_64 altman
 apt-get install mono-complete libgdiplus gtk-sharp2 -y
 cat > /usr/share/applications/altman.desktop <<EOF
 [Desktop Entry]
