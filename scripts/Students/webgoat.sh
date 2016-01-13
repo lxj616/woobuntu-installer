62ed13b2cdbbd58463587d494d58536716ecbd35
lxj616 <502307678@qq.com>
2016-01-13 12:53:55 +0800

    fix webgoat - lxj616

--- scripts/Students/webgoat.sh
+++ scripts/Students/webgoat.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
+rm -rf /opt/woobuntu/webgoat
 mkdir -p /opt/woobuntu/webgoat
 cd /opt/woobuntu/webgoat
-wget https://github.com/WebGoat/WebGoat-Legacy/releases/download/v6.0.1/WebGoat-6.0.1-war-exec.jar -o webgoat.jar
+wget https://github.com/WebGoat/WebGoat-Legacy/releases/download/v6.0.1/WebGoat-6.0.1-war-exec.jar -O webgoat.jar
 cat > /usr/share/applications/webgoat.desktop <<EOF
 [Desktop Entry]
 Version=1.0
 Type=Application
 Name=webgoat
 Icon=application-default-icon
-Exec=xfce4-terminal -e '/bin/bash -c "cd /opt/woobuntu/webgoat;java -jar webgoat.jar; exec bash"'
+Exec=xfce4-terminal -e '/bin/bash -c "cd /opt/woobuntu/webgoat;sudo java -jar webgoat.jar; exec bash"'
 NoDisplay=false
 Categories=woobuntu;
 StartupNotify=true
