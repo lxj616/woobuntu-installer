a881f9424af98969bca44460e9c31c8802ce00aa
lxj616 <502307678@qq.com>
2016-01-19 11:20:36 +0800

    QQ 7.8 should not install as root - lxj616

--- scripts/Utils/wineqq-7.8.sh
+++ scripts/Utils/wineqq-7.8.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 cd /tmp
-wget http://www.longene.org/download/WineQQ7.8-20151109-Longene.deb
-dpkg -i WineQQ7.8-20151109-Longene.deb
-apt-get -f install
+firefox http://www.longene.org/download/WineQQ7.8-20151109-Longene.deb
