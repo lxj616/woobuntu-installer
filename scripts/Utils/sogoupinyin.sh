fc87c23cfb66e8db04350eba28885b394a036e97
lxj616 <502307678@qq.com>
2016-06-20 10:26:49 +0800

    update sogoupinyin up-to-date - lxj616

--- scripts/Utils/sogoupinyin.sh
+++ scripts/Utils/sogoupinyin.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /tmp
-wget http://cdn2.ime.sogou.com/dl/index/1446541585/sogoupinyin_2.0.0.0068_amd64.deb
-sudo dpkg -i sogoupinyin_2.0.0.0068_amd64.deb
-rm sogoupinyin_2.0.0.0068_amd64.deb
+wget http://cdn2.ime.sogou.com/dl/index/1465191139/sogoupinyin_2.0.0.0078_amd64.deb
+sudo dpkg -i sogoupinyin_2.0.0.0078_amd64.deb
+rm sogoupinyin_2.0.0.0078_amd64.deb
 sudo apt-get -f install -y
 
